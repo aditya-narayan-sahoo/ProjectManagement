5bc4200b02a6807b99d3c4d5edb6204f6bb76fa6
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-08-11 06:55:18 +0530

    Add health check functions to project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import express from "express";
 
 import connectDB from "./utils/db.js";
 
+import healthCheckRouter from "./routes/healthcheck.route.js";
+
 const PORT = process.env.PORT || 5432;
 
 const app = express();
@@ -19,12 +21,19 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public")); // serve static files
 app.use(express.urlencoded({ extended: false, limit: "16kb" }));
 
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 app.get("/", (req, res) => {
   res.send("Starting a backend project");
 });
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
   });
-});
