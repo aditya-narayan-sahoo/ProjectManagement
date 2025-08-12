a7a5d88c30e2946bac9cc237870d8edb488655aa
Aditya Narayan Sahoo <aditysahoo246@gmail.com>
2025-08-12 23:23:09 +0530

    Add hooks and functions to user model

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on http://localhost:${PORT}`);
     });
   })
   .catch((error) => {
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
+import crypto from "crypto";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { model, Schema } from "mongoose";
 
 const userSchema = new Schema(
@@ -67,5 +69,35 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { id: this._id, username: this.username, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unhashedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unhashedToken)
+    .digest("hex");
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20 minutes in milliseconds
+  return { unhashedToken, hashedToken, tokenExpiry };
+};
 
 export const User = model("User", userSchema);
